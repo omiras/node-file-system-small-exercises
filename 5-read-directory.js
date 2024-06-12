e3f33f060b13a40ac6462030ac440035fe601d11
omiras <oscar.miras@gmail.com>
2024-06-12 13:50:47 +0200

    done

--- 5-read-directory.js
+++ 5-read-directory.js
@@ -1,3 +1,13 @@
 /**
  * Crea un script que muestre por consola cada uno de los archivos del directorio 'backup-files'
  */
+
+const fs = require('fs');
+
+// Synchronize listing of files in a directory
+const files = fs.readdirSync('./backup-files');
+console.log("Todos los ficheros en formato array: ", files);
+
+for (f of files) {
+    console.log(f);
+}
--- 6-read-and-replace.js
+++ 6-read-and-replace.js
@@ -1,9 +1,12 @@
 /**
  * Crea una función que lea un fichero que remplaze todos los textos de un fichero. Se debe generar un nuevo fichero resultante de nombre "result.html"   
  */
+const fs = require('fs');
 
 function replaceTextInFile(htmlFilePath, textToSearch, textToReplace) {
-
+    const fileContent = fs.readFileSync(htmlFilePath, "utf-8");
+    const replacedContent = fileContent.replaceAll(textToSearch, textToReplace);
+    fs.writeFileSync("result.html", replacedContent);
 }
 
 replaceTextInFile("./index.html", "FFC371", "ADD8E6");
