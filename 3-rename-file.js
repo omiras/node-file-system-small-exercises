0974f1bc59f16b3ba410e4b35a6c42f58f464176
omiras <oscar.miras@gmail.com>
2024-06-12 16:18:12 +0200

    sol

--- 3-rename-file.js
+++ 3-rename-file.js
@@ -1,3 +1,11 @@
 /**
  * Renombra el fichero "bird.jpg" a "pajaro.jpg"
  */
+const fs = require('fs')
+
+fs.rename('./bird.jpg', './pajaro.jpg', (err) => {
+
+    if (err) {
+        console.log("Ha ocurrido un error.", err);
+    }
+}) 
--- 4-remove-file.js
+++ 4-remove-file.js
@@ -1,3 +1,6 @@
 /**
  * Borra el fichero 'borrame.txt' situado en este mismo directorio
  */
+const fs = require('fs');
+
+fs.unlinkSync("./borrame.txt");
